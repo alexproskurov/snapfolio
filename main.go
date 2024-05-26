52bf6ae1e594fdca65f6749942f7c6f0026a6889
Alex Proskurov <alexproskurov97@gmail.com>
2024-05-26 00:40:32 -0600

    feat: add tailwind CSS layout

--- main.go
+++ main.go
@@ -16,14 +16,20 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "home.gohtml"))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"tailwind.gohtml", "home.gohtml",
+	))))
 
-	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "contact.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"tailwind.gohtml", "contact.gohtml",
+	))))
 
-	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "faq.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
+		templates.FS,
+		"tailwind.gohtml", "faq.gohtml",
+	))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
