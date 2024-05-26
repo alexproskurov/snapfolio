882687ff972f61dd5ac95ad1632cbf4d2ba0185e
Alex Proskurov <alexproskurov97@gmail.com>
2024-05-25 20:50:12 -0600

    feat: embed the templates and add support for parsing embedded templates

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/alexproskurov/web-app/controllers"
+	"github.com/alexproskurov/web-app/templates"
 	"github.com/alexproskurov/web-app/views"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -15,16 +16,14 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	
-
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
--- views/template.go
+++ views/template.go
@@ -3,6 +3,7 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
@@ -15,14 +16,25 @@ func Must(t Template, err error) Template {
 	return t
 }
 
-func Parse(filepath string) (Template, error) {
-	htmlTpl, err := template.ParseFiles(filepath)
+func ParseFS(fs fs.FS, pattern string) (Template, error) {
+	tpl, err := template.ParseFS(fs, pattern)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
 
 	return Template{
-		htmlTpl: htmlTpl,
+		htmlTpl: tpl,
+	}, nil
+}
+
+func Parse(filepath string) (Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+
+	return Template{
+		htmlTpl: tpl,
 	}, nil
 }
 
@@ -32,7 +44,7 @@ type Template struct {
 
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("content-Type", "text/html; charset=utf-8")
-	
+
 	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
