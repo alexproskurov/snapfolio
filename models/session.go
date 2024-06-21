303b38d386f95b823f8e83c98d4376b1347c1357
Alex Proskurov <alexproskurov97@gmail.com>
2024-06-21 17:48:06 -0600

    refactor: single SQL query for user lookup

--- models/session.go
+++ models/session.go
@@ -59,22 +59,12 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 func (ss *SessionService) User(token string) (*User, error) {
 	tokenHash := ss.TokenManager.Hash(token)
 	var user User
-	// Query for the session using hashed token.
 	row := ss.DB.QueryRow(`
-		SELECT user_id 
-		FROM sessions 
-		WHERE token_hash = $1`, tokenHash)
-	err := row.Scan(&user.ID)
-	if err != nil {
-		return nil, fmt.Errorf("user session: %w", err)
-	}
-
-	// Using the user ID from the session to query for that user.
-	row = ss.DB.QueryRow(`
-		SELECT email, password_hash 
-		FROM users 
-		WHERE id = $1;`, user.ID)
-	err = row.Scan(&user.Email, &user.PasswordHash)
+		SELECT users.id, users.email, users.password_hash 
+		FROM sessions
+		JOIN users ON users.id = sessions.user_id
+		WHERE sessions.token_hash = $1;`, tokenHash)
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash)
 	if err != nil {
 		return nil, fmt.Errorf("user session: %w", err)
 	}
