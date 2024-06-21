0fee9f008e5aad1b14e42b1975c97197a7c64cb3
Alex Proskurov <alexproskurov97@gmail.com>
2024-06-21 17:47:33 -0600

    refactor: add the foreign key to the sessions table and delete sessions when the corresponding user is deleted

--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -1,5 +1,7 @@
 CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE,
-    token_hash TEXT UNIQUE NOT NULL
+    user_id INT UNIQUE ON DELETE CASCADE,
+    token_hash TEXT UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
 );
