5cfb04cbfd0bfcb6ecd55749b67704bbc084ad6d
Alex Proskurov <alexproskurov97@gmail.com>
2024-07-14 19:57:13 -0600

    feat: add models/errors.go to store error messages

---
+++ models/errors.go
@@ -0,0 +1,8 @@
+package models
+
+import "errors"
+
+var (
+	ErrEmailTaken       = errors.New("models: email address is already in use")
+	ErrUserDoesNotExist = errors.New("models: user with provided email address does not exist")
+)
