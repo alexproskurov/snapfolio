eb6d5dcee9e93ba96229c24bbbde7cba4d71b8a0
Alex Proskurov <alexproskurov97@gmail.com>
2024-07-15 18:54:31 -0600

    feat: add gallery model and migration

--- models/errors.go
+++ models/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrEmailTaken       = errors.New("models: email address is already in use")
 	ErrUserDoesNotExist = errors.New("models: user with provided email address does not exist")
+	ErrNotFound         = errors.New("models: resource could not be found")
 )
